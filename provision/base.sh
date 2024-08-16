f47725ae4f14028e6ffa191aec5254d8437d9e8b
Kirill Trofimov <g3rhard@users.noreply.github.com>
2024-08-16 06:56:16 +0200

    provision/base.sh: optimize installation

--- provision/base.sh
+++ provision/base.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
+set -e
 
-# Base
-sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install git-core software-properties-common python python-setuptools -y
-sudo DEBIAN_FRONTEND=noninteractive apt-add-repository -y ppa:ansible/ansible
-sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -qq install -y ansible
+# Set DEBIAN_FRONTEND environment variable
+export DEBIAN_FRONTEND=noninteractive
+
+# Update and upgrade the system
+sudo -E apt-get update -qq
+sudo -E apt-get upgrade -yqq
+
+# Install required packages in one step
+sudo -E apt-get install -yqq git-core software-properties-common python python-setuptools
+
+# Add Ansible PPA and install Ansible
+sudo -E apt-add-repository -y ppa:ansible/ansible
+sudo -E apt-get update -qq
+sudo -E apt-get install -yqq ansible
+
+# Clean up unnecessary files to reduce the AMI size
+sudo -E apt-get clean
+sudo -E rm -rf /var/lib/apt/lists/*
